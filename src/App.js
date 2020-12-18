6ef423adbf8327df97ac78569dbf6a293d9f3e08
habib610 <habib54562@gmail.com>
2020-12-19 00:31:25 +0600

    homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+   <header  className="row">
+     <div>
+<a href="#" className="brand">Amazona</a>
+     </div>
+     <div><a href="#">Cart</a>
+     <a href="#">SignIn</a>
+     </div>
+   </header>
+   <main className="row center">
+    {
+      data.products.map(product => (
+        <div className="card" key={product._id} >
+         <a href={`product/${product._id}`}>
+           <img src={product.image} alt={product.name} className="medium"/>
+         </a>
+         <div className="card-body">
+           <a href={`product/${product._id}`}>
+           <h2>{product.name}</h2>
+           </a>
+           <div className="rating">
+           <span><i class="fa fa-star"></i></span>
+           <span><i class="fa fa-star"></i></span>
+           <span><i class="fa fa-star"></i></span>
+           <span><i class="fa fa-star"></i></span>
+           <span><i class="fa fa-star"></i></span>
+           </div>
+           <div className="price">
+             $ {product.price}
+           </div>
+           </div>
+       </div>
+      ))
+    }
+   </main>
+   <footer class="row center">
+            All Right Reserved
+        </footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products : [
+        {
+            _id: 1,
+            name: 'Awesome Shirt',
+            category: 'Shirts',
+            image: 'images/s2.jpg',
+            price: 110,
+            brand: 'Panda',
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shirts"
+        },
+        {
+            _id: 2,
+            name: 'Outstanding Shirt',
+            category: 'Shirts',
+            image: 'images/s1.jpg',
+            price: 120,
+            brand: 'Marvel',
+            rating: 4.5,
+            numReviews: 7,
+            description: "high quality shirts"
+        },
+        {
+            _id: 3,
+            name: 'Trending Shirt',
+            category: 'Shirts',
+            image: 'images/s3.jpg',
+            price: 130,
+            brand: 'Panda',
+            rating: 4.5,
+            numReviews: 8,
+            description: "high quality shirts"
+        },
+        {
+            _id: 4,
+            name: 'Gorgeous Shirt',
+            category: 'Shirts',
+            image: 'images/s4.jpg',
+            price: 110,
+            brand: 'Ninja',
+            rating: 5,
+            numReviews: 12,
+            description: "high quality shirts"
+        },
+        {
+            _id: 5,
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: 'images/s5.jpg',
+            price: 210,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality shirts"
+        },
+        {
+            _id: 6,
+            name: 'Nike Shirts',
+            category: 'Shirts',
+            image: 'images/s6.jpg',
+            price: 100,
+            brand: 'Gorgia',
+            rating: 4,
+            numReviews: 17,
+            description: "high quality shirts"
+        }
+    ]
+}
+
+export default data;
